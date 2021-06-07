f89d624b2bbc12fcc470dec689c8925bafdbd939
Marcin Czyszczon <marcin.czyszczon@diehl.com>
2021-06-07 13:27:38 +0200

    new files third trainings day

--- test_framework/test_login.py
+++ test_framework/test_login.py
@@ -2,11 +2,13 @@ import pytest
 import requests
 
 URL = 'https://reqres.in/api/login'
+RESPONSE_OK = 200
+RESPONSE_NOK = 400
 
 
 @pytest.mark.parametrize('body', [{'email': 'eve.holt@reqres.in', 'password': 'cityslicka'}])
 def test_login_ok(body):
-    assert requests.post(URL, json=body).status_code == 200
+    assert requests.post(URL, json=body).status_code == RESPONSE_OK
     assert requests.post(URL, json=body).json()['token']
 
 
@@ -17,5 +19,5 @@ def test_login_ok(body):
                                   {'email': '', 'password': 'cityslicka'},
                                   {'password': 'cityslicka'}])
 def test_login_nok(body):
-    assert requests.post(URL, json=body).status_code == 400
+    assert requests.post(URL, json=body).status_code == RESPONSE_NOK
     assert 'token' not in requests.post(URL, json=body).json()
